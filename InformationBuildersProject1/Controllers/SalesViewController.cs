41b60465d42f3c14b5ce8d53c31e3a81710877b8
mkavidas <michael.kavidas@gmail.com>
2019-01-11 17:07:45 -0500

    Connection string, simple mvc with simple table view

---
+++ InformationBuildersProject1/Controllers/SalesViewController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using InformationBuildersProject1.Models;
+
+namespace InformationBuildersProject1.Controllers
+{
+    public class SalesViewController : Controller
+    {
+        // GET: SalesView
+        public ActionResult Index()
+        {
+            return View(LoadData());
+        }
+        //This function loads data from sql server into a list of SalesViewDetails
+        private List<SalesViewDetails> LoadData()
+        {
+            List<SalesViewDetails> sales = new List<SalesViewDetails>();
+
+            String sql = "SELECT * FROM simple_sales_view";
+
+            string strCon = System.Web
+                                  .Configuration
+                                  .WebConfigurationManager
+                                  .ConnectionStrings["wf_retail"].ConnectionString;
+
+            SqlConnection conn = new SqlConnection(strCon);
+            SqlCommand comm = new SqlCommand(sql, conn);
+            conn.Open();
+            SqlDataReader nwReader = comm.ExecuteReader();
+            while (nwReader.Read())
+            {
+                DateTime cur = (DateTime)nwReader["TIME_DATE"];
+                sales.Add(new SalesViewDetails
+                {
+                    TIME_DATE = cur.ToString("yyyy/MM/dd "),
+                    STATE_PROV_CODE_ISO_3166_2 = (string)nwReader["STATE_PROV_CODE_ISO_3166_2"],
+                    STORE_TYPE = (string)nwReader["STORE_TYPE"],
+                    BRANDTYPE = (string)nwReader["BRANDTYPE"],
+                    BRAND = (string)nwReader["BRAND"],
+                    MODEL = (string)nwReader["MODEL"],
+                    QUANTITY_SOLD = (int)nwReader["QUANTITY_SOLD"],
+                    REVENUE_US = Convert.ToDouble(nwReader["REVENUE_US"])
+                });
+            }
+            nwReader.Close();
+            conn.Close();
+
+
+            return sales;
+        }
+    }
+}
---
+++ InformationBuildersProject1/Models/SalesViewDetails.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace InformationBuildersProject1.Models
+{
+    public class SalesViewDetails
+    {
+        public string TIME_DATE { get; set; }
+        public string STATE_PROV_CODE_ISO_3166_2 { get; set; }
+
+        public string STORE_TYPE { get; set; }
+        public string BRANDTYPE { get; set; }
+        public string BRAND { get; set; }
+        public string MODEL { get; set; }
+        public int QUANTITY_SOLD { get; set; }
+        public double REVENUE_US { get; set; }
+    }
+}
