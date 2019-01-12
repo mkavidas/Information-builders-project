d82e2fba7d82b721708552b325f8f1e4e5227a54
mkavidas <michael.kavidas@gmail.com>
2019-01-11 19:11:59 -0500

    removed useless pages and added information

--- InformationBuildersProject1/Controllers/HomeController.cs
+++ InformationBuildersProject1/Controllers/HomeController.cs
@@ -15,14 +15,14 @@ namespace InformationBuildersProject1.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
+            ViewBag.Message = "application description page.";
 
             return View();
         }
 
         public ActionResult Contact()
         {
-            ViewBag.Message = "Your contact page.";
+            ViewBag.Message = "contact page.";
 
             return View();
         }
